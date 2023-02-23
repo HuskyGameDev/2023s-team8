54a35053dc0b887be9750037ac777b4143cae3d3
Npsantos <npsantos@mtu.edu>
2023-02-22 21:56:20 -0500

    Added tile puzzle sliding mechanism

---
+++ HGD Enigma Box/Assets/Scripts/TilePuzzle.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TilePuzzle : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace = null;
+    private Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit) {
+                if(Vector2.Distance(emptySpace.position, hit.transform.position) < 2) {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    emptySpace.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpacePosition;
+                }
+            }
+        }
+    }
+}
