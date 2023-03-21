99d40cf6a79ba430ad93dd3f6279db33deeb2aec
ZientDeveero <jmpigeon@mtu.edu>
2023-03-21 10:34:34 -0400

    Prefab for interactable objects done
    I put in the comments of the main menu code what it does, hopefully it's enough explanation for how to use it

--- HGD Enigma Box/Assets/Scripts/LevelSelectScript.cs
+++ HGD Enigma Box/Assets/Scripts/LevelSelectScript.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+/**
+Just a comment for anyone trying to make a scene that can be travelled to:
+If you need to travel to a scene, make sure to add the scene to the build manager
+and DO NOT PUT IT BEFORE THE LEVELS, otherwise I have to change all of the buttons
+to go to the correct scene
+*/
 public class LevelSelectScript : MonoBehaviour
 {
     public int level;
--- HGD Enigma Box/Assets/Scripts/MainMenu.cs
+++ HGD Enigma Box/Assets/Scripts/MainMenu.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+/**
+Just a comment for anyone trying to make a scene that can be travelled to:
+If you need to travel to a scene, make sure to add the scene to the build manager
+and DO NOT PUT IT BEFORE THE PRIMARY LEVELS, otherwise I have to change all of the buttons
+to go to the correct scene. The secondary levels are fine because of my implementation
+*/
 public class MainMenu : MonoBehaviour
 {
-    //I make a enum here so that what I'm adding is the number that I need to get to a certain scene
-    enum Scenes { 
-        LevelSelect = 1
-    }
+    
+    /**
+    Level select is the next scene, so I have it be one.
+    Secondary will be given by the object pressed.
+    Depending on which object was pressed, it will give the
+    number needed to get the it's corresponding scene
+    */
+    private int LevelSelect = 1;
+    public string Secondary;
     //when the start button is pressed, send us to level select
     public void Play() {
         //So we're at scene 0 currently, I add by LevelSelect, (which is 1), to get me to the level select scene which is scene 1
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + (int)Scenes.LevelSelect);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + (int)LevelSelect);
     }
     //Code to send to options menu
     public void OptionMenu() {
         SceneManager.LoadScene("Options");
     }
+    //Code will do what it's supposed to when we make an executable, the debug statement is there to show that it does
+    //Go in here
     public void Exit() {
         Application.Quit();
         Debug.Log("Off to visit your mother");
+    }
+    //Similar to level select where the button gives a number to send to
+    public void GoToSecondary(){
+        SceneManager.LoadScene("Secondary Level " + Secondary);
     }
 }
