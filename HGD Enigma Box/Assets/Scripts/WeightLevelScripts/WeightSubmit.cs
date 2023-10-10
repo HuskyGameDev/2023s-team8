2708d08b9563b1d937aba2affc1a867a0bd500a4
Mack Abercrombie <Mdabercr@mtu.edu>
2023-10-10 11:12:54 -0400

    Weight level can now be completed
    Got the submit button to reload the scene or go back to level select. Now just need to make it look nicer, possibly add animation for the catapult part of it.

--- HGD Enigma Box/Assets/Scripts/WeightLevelScripts/WeightSubmit.cs
+++ HGD Enigma Box/Assets/Scripts/WeightLevelScripts/WeightSubmit.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 //there will be a submit button and this will go to that to check the weights when submit is pressed
 
-public class Test : MonoBehaviour
+public class WeightSubmit : MonoBehaviour
 {
     //Total weight based on what rocks are selected
     [SerializeField] int total;
@@ -41,16 +42,29 @@ public class Test : MonoBehaviour
         {
             //level will end succesfully
             Debug.Log("Correct weight");
+            Invoke("LoadLevelSelect", 3f);
         } else if(total < 7)
         {
             //level ends with not enough weight
             Debug.Log("Not enough weight");
+            Invoke("ReloadScene", 3f);
         } else 
         {
             //level ends with too much weight
             Debug.Log("Too much weight");
+            Invoke("ReloadScene", 3f);
         }
         //For now will just reset total for testing
         total = 0;
+    }
+
+    public void LoadLevelSelect()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
