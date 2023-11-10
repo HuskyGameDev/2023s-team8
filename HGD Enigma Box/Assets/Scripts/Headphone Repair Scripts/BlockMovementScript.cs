62740812ee2367501869c0a096a903e6a03a7887
ZientDeveero <jmpigeon@mtu.edu>
2023-11-09 20:52:05 -0500

    Update for headphone level
    Able to move the pieces and they snap back to starting position

--- HGD Enigma Box/Assets/Scripts/Headphone Repair Scripts/BlockMovementScript.cs
+++ HGD Enigma Box/Assets/Scripts/Headphone Repair Scripts/BlockMovementScript.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class BlockMovementScript : MonoBehaviour
 {
     private bool dragging;
+    private Vector2 offset, originalPosition;
+    public void Awake()
+    {
+        originalPosition = transform.position;
+    }
     private void OnMouseDown()
     {
         dragging = true;
+        offset = getMousePos() - (Vector2)transform.position;
+    }
+    private void OnMouseUp()
+    {
+        transform.position = originalPosition;
+        dragging = false;
     }
     private void Update()
     {
         if (!dragging) {
             return;
         }
-        Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePosition;
+        Vector2 mousePosition = getMousePos();
+        transform.position = mousePosition - offset;
+    }
+    Vector2 getMousePos() {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
