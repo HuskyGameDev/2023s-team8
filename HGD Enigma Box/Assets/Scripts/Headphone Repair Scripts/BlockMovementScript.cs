45501e75e470d535fd4b1f2a85b11fca36067838
ZientDeveero <jmpigeon@mtu.edu>
2023-11-09 20:20:11 -0500

    New attempt at dragging and dropping
    The old implementation seemed like it would be hard to get objects to interact with each other easily so I changed tactics

--- HGD Enigma Box/Assets/Scripts/Headphone Repair Scripts/BlockMovementScript.cs
+++ HGD Enigma Box/Assets/Scripts/Headphone Repair Scripts/BlockMovementScript.cs
@@ -3,51 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockMovementScript : MonoBehaviour
-{ 
-    public GameObject selectedObject;
-    Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
+{
+    private bool dragging;
+    private void OnMouseDown()
     {
-        
+        dragging = true;
     }
-
-    // Update is called once per frame
-        
-    void Update()
+    private void Update()
     {
-        //get the position of the mouse based on the camera
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //if left mouse click is pressed
-        if (Input.GetMouseButtonDown(0))
-        {
-            //check to see if a collider exists where the mouse positions is
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-            //if a collider object exists
-            if (targetObject)
-            {
-                //get the parent game object of the collider
-                selectedObject = targetObject.transform.gameObject;
-                /*this offset is used to make sure the selected object doesn't go behind the screen
-                 * it normally will make the object disappear becausue the mouse position will be
-                 * calculated using the camera, making the z-position at the camera and make the object
-                 * disappear. 
-                 * 
-                 * TLDR: the object won't disappear when clicked on
-                 */
-                offset = selectedObject.transform.position - mousePosition;
-            }
+        if (!dragging) {
+            return;
         }
-        //if there is an object that is being selected
-        if (selectedObject)
-        {
-            //drag the object to the right spot
-            selectedObject.transform.position = mousePosition + offset;
-        }
-        //if the left mouse button is let go and there was an object, set it back to null
-        if (Input.GetMouseButtonUp(0) && selectedObject)
-        {
-            selectedObject = null;
-        }
+        Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
     }
 }
