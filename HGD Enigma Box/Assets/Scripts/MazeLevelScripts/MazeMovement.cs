1bdbe4c6b212c4fc3ac4b0f289d5cb39e66421f5
Mack Abercrombie <Mdabercr@mtu.edu>
2023-11-28 10:28:42 -0500

    Fixed UI elements
    Initially either the background would cover the grid or the grid would cover the back button. That has now been fixed.

--- HGD Enigma Box/Assets/Scripts/MazeLevelScripts/MazeMovement.cs
+++ HGD Enigma Box/Assets/Scripts/MazeLevelScripts/MazeMovement.cs
@@ -14,6 +14,9 @@ public class MazeMovement : MonoBehaviour
     //Used for movement of the player
     Rigidbody2D rig;
 
+    //Boolean for if the player has reached the end to stop movement.
+    bool finished = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,48 +31,55 @@ public class MazeMovement : MonoBehaviour
         float right = 0f;
         float up = 0f;
         float down = 0f;
-        
-        if (Input.GetKey(KeyCode.W))
+        if (!finished)
         {
-            up = speed;
-            //Debug.Log("W pressed");
+            if (Input.GetKey(KeyCode.W))
+            {
+                up = speed;
+                //Debug.Log("W pressed");
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                left = -speed;
+                //Debug.Log("A pressed");
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                down = -speed;
+                //Debug.Log("S pressed");
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                right = speed;
+                //Debug.Log("D pressed");
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                up = 0f;
+                //Debug.Log("W released");
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                left = 0f;
+                //Debug.Log("A released");
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                down = 0f;
+                //Debug.Log("S released");
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                right = 0f;
+                //Debug.Log("D released");
+            }
+            rig.velocity = new Vector2(left + right, up + down);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (finished) 
         {
-            left = -speed;
-            //Debug.Log("A pressed");
+            //Will be changed once know what value to use in end level
+            Invoke("ReloadScene", 3f);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            down = -speed;
-            //Debug.Log("S pressed");
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            right = speed;
-            //Debug.Log("D pressed");
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            up = 0f;
-            //Debug.Log("W released");
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            left = 0f;
-            //Debug.Log("A released");
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            down = 0f;
-            //Debug.Log("S released");
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            right = 0f;
-            //Debug.Log("D released");
-        }
-        rig.velocity = new Vector2(left + right, up + down);
     }
 
     //For detection of player hitting walls
@@ -79,13 +89,14 @@ public class MazeMovement : MonoBehaviour
         //if end has collider then player cannot go through it
         if (collision.gameObject.tag == "Finish")
         {
+            finished = true;
             Debug.Log("Reached finish!");
         }
     }
 
     public void EndLevel()
     {
-        gameManager.pogressMade(2);
+        //gameManager.pogressMade(2);
     }
 
     public void ReloadScene()
