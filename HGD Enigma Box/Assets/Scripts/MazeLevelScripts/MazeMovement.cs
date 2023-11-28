00bfac3817f3e04954466f6918b6f35531099b47
Mack Abercrombie <Mdabercr@mtu.edu>
2023-11-28 17:20:01 -0500

    Cleaned up code and changed positioning
    Cleaned my code to make it more readable and changed the positioning of the player to make the maze longer.

--- HGD Enigma Box/Assets/Scripts/MazeLevelScripts/MazeMovement.cs
+++ HGD Enigma Box/Assets/Scripts/MazeLevelScripts/MazeMovement.cs
@@ -9,7 +9,7 @@ public class MazeMovement : MonoBehaviour
     public GameManager gameManager;
 
     //Speed of the player movement
-    private float speed = 5f;
+    private float speed = 10f;
 
     //Used for movement of the player
     Rigidbody2D rig;
@@ -31,47 +31,40 @@ public class MazeMovement : MonoBehaviour
         float right = 0f;
         float up = 0f;
         float down = 0f;
+
         if (!finished)
         {
             if (Input.GetKey(KeyCode.W))
             {
                 up = speed;
-                //Debug.Log("W pressed");
             }
             if (Input.GetKey(KeyCode.A))
             {
                 left = -speed;
-                //Debug.Log("A pressed");
             }
             if (Input.GetKey(KeyCode.S))
             {
                 down = -speed;
-                //Debug.Log("S pressed");
             }
             if (Input.GetKey(KeyCode.D))
             {
                 right = speed;
-                //Debug.Log("D pressed");
             }
             if (Input.GetKeyUp(KeyCode.W))
             {
                 up = 0f;
-                //Debug.Log("W released");
             }
             if (Input.GetKeyUp(KeyCode.A))
             {
                 left = 0f;
-                //Debug.Log("A released");
             }
             if (Input.GetKeyUp(KeyCode.S))
             {
                 down = 0f;
-                //Debug.Log("S released");
             }
             if (Input.GetKeyUp(KeyCode.D))
             {
                 right = 0f;
-                //Debug.Log("D released");
             }
             rig.velocity = new Vector2(left + right, up + down);
         }
@@ -86,22 +79,25 @@ public class MazeMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //To check when player reaches end
-        //if end has collider then player cannot go through it
         if (collision.gameObject.tag == "Finish")
         {
             finished = true;
-            //Debug.Log("Reached finish!");
+            
             //sets the player position to the center of the finish just to make it look slightly better
             rig.constraints = RigidbodyConstraints2D.FreezeAll;
             rig.position = new Vector2(26.25f, 16.74f);
         }
     }
 
+    //Will end the level in the proper way
     public void EndLevel()
     {
+        //Will either use gameManager if the level is treated like a primary level
+        //or will just go to level select if the level is treated like a secondary level
         //gameManager.pogressMade(2);
     }
 
+    //Used right now for the finish until secondary levels decided
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
