b55932e8d72f0c626a1e4092f3f0ab9f8ca5f278
Matthew Dunham <mrdunham@mtu.edu>
2023-11-28 17:59:55 -0500

    Finished Drag Demo for Matthew Buckeridge
    This is the full implementation of the drag demo with all the comments necessary to see how this works.

--- HGD Enigma Box/Assets/Scripts/Testing/BoxController.cs
+++ HGD Enigma Box/Assets/Scripts/Testing/BoxController.cs
@@ -1,23 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxController : MonoBehaviour {
     public LevelController levelController;
-    private SpringJoint2D springJoint;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rigidBody2D;
 
+    /**
+     * This is simply making referencing the object's regidbody easier and saving some computation.
+     * Additionally making sure that all boxes have the proper freeze tags enabled.
+    */
     private void Start() {
-        this.springJoint = this.GetComponentInParent<SpringJoint2D>();
-        this.rigidbody = this.GetComponentInParent<Rigidbody2D>();
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+        rigidBody2D.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
+    /**
+     * When the mouse is pressed ontop of a box this method is called.
+     */
     private void OnMouseDown() {
-        levelController.Move(this.rigidbody);
+        levelController.Move(rigidBody2D);                                  //Set the rigidbody to be moved.
     }
 
+    /**
+     * When the mouse is released ontop of a box this method is called.
+     */
     private void OnMouseUp() {
-        levelController.Release();
-        rigidbody.velocity = Vector2.zero;
+        levelController.Release();                                          //Release the currently held box.
+    }
+
+    /**
+     * This method is optional but I included it due to you wanting the boxes to be dropped when they collided, at least from my understanding.
+     * This method is called when the box makes contact with another collider.
+     * Note that in further implementation this may have to be tweaked to work properly, just contact me if you need help.
+     */
+    private void OnCollisionEnter2D(Collision2D collision) {
+        levelController.Release();                                          //Release the currently held box
     }
 }
--- HGD Enigma Box/Assets/Scripts/Testing/LevelController.cs
+++ HGD Enigma Box/Assets/Scripts/Testing/LevelController.cs
@@ -1,48 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour {
     public Vector2 mousePoint;
-    private Vector2 velocity;
+    private Vector2 vel;
     private Rigidbody2D curr;
 
+    /**
+     * The contents of this method are handling the movement of the box.
+     */
     private void FixedUpdate() {
-        mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (curr) {
-            velocity = curr.velocity;
-            curr.position = Vector2.SmoothDamp(curr.position, mousePoint, ref velocity, 0.05f);
+        if (curr) {                 //Make sure that a box is currently selected by seeing if curr is null, if it is not then it is interpreted as "true" by the statement.
+            vel = curr.velocity;    //Save the current box velocity as a Vector2 so it can be referenced.
+            mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);                   //Save the in-game position of the mouse only on a 2D plane since this is a Vector2.
+            curr.MovePosition(Vector2.SmoothDamp(curr.position, mousePoint, ref vel, 0.025f));  //Just look at the documentation to see how this line works; in summary it moves the box with the previously gathered information.
         }
     }
 
+    //Side note... the comment below looks ugly but is required so the decription is shown when hovering over the method name and its parameter.
+
+    /// <summary>
+    /// This method sets the rigidbody to be moved.
+    /// </summary>
+    /// <param name="rigidbody">
+    /// The rigidbody that is going to be moved.
+    /// </param>
     public void Move(Rigidbody2D rigidbody) {
         curr = rigidbody;
-        //velocity = rigidbody.velocity;
-        //rigidbody.position = Vector2.SmoothDamp(rigidbody.position, mousePoint, ref velocity, 0.01f);
+        curr.constraints = RigidbodyConstraints2D.FreezeRotation;   //Remove position constraints so that the box can now be moved but still cannot be rotated.
     }
 
+    /// <summary>
+    /// This method releases the currently held object.
+    /// </summary>
     public void Release() {
-        Debug.Log("Released!");
-        curr = null;
-    }
-}
-
-/*public class LevelController : MonoBehaviour {
-    public Rigidbody2D mousePoint;
-
-    private void FixedUpdate() {
-        mousePoint.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-    }
-
-    public void Move(SpringJoint2D springJoint) {
-        if (!springJoint.connectedBody) {
-            Debug.Log("Moving " + springJoint);
-            springJoint.connectedBody = mousePoint;
+        if (curr) {
+            curr.constraints = RigidbodyConstraints2D.FreezeAll;    //Add constraints to freeze all forms of movement when the box is not held.
+            curr = null;
         }
     }
-
-    public void Release(SpringJoint2D springJoint) {
-        Debug.Log("Released!");
-        springJoint.connectedBody = null;
-    }
-}*/
+}
